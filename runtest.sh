bd860a9bcfca11e6e6d7c1d74a5bf14ba81b52c2
abhitosh25 <73384822+abhitosh25@users.noreply.github.com>
2020-11-23 01:15:40 +0530

    Add files via upload

--- runtest.sh
+++ runtest.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
+#Declare Variables
 time=$(date "+%Y.%m.%d-%H.%M")
 ScriptName=SampleEcommerce.jmx
 ToEmail=abhitosh.qa@gmail.com
 #htmlName=htmlreport_$time
 #jtlName=jtl_$time.jtl
 results=PerfTest_results_$time
+#Download JMeter
 #java -version
 #curl http://mirror.cc.columbia.edu/pub/software/apache/jmeter/binaries/apache-jmeter-5.3.tgz -o apache-jmeter-5.3.tgz;
 #tar -xvzf apache-jmeter-5.3.tgz;
-cd apache-jmeter-5.3/bin;
+#Create Test Directories
 mkdir ../../$results
 mkdir ../../$results/HTMLReport
 mkdir ../../$results/Errors
 mkdir ../../$results/InputData
+#Execute Performance Test
+cd apache-jmeter-5.3/bin;
 sh jmeter.sh -Jjmeter.save.saveservice.output_format=xml -Jjmeter.save.saveservice.output_format=csv -n -t ../../$ScriptName -l ../../$results/Test.jtl -e -o ../../$results/HTMLReport
+##Collect Test Artifacts
 mv jmeter.log ../../$results
 mv ../../PerfError_* ../../$results/Errors
 mv ../../Input_*.csv ../../$results/InputData
 cp ../../$results/Test.jtl ../../$results/AggregateReport.csv
 mv ../../$results/HTMLReport/index.html ../../$results/HTMLReport/PerfTestReport.html
+#Mail The Results
 #mailx -a ../../$results/HTMLReport/PerfTestReport.html -s "Performance Testing Report" $ToEmail < /dev/null
